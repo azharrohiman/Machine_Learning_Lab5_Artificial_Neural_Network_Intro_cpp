7a2aa9c3279a031fce54fe502358436bfc4412eb
Azhar Rohiman <rhmmuh005@myuct.ac.za>
2018-09-05 16:03:51 +0200

    Initial commit

---
+++ driver.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+#include "NeuralNetwork.h"
+
+int main(int argc, char* argv[]) {
+
+	using namespace std;
+
+	RHMMUH005::NeuralNetwork neural1;
+
+	cout << &neural1 << endl;
+
+	return 0;
+}
